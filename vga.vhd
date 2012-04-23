73c4096bd074a3c34dee1d603eb5053a5cab2e4b
Nepta <586f2e4c56@gmail.com>
2012-04-23 14:36:45 +0200

    instanciated THE snake! (only the head)

--- vga.vhd
+++ vga.vhd
@@ -7,9 +7,7 @@ entity vga is
 		clk, raza: in std_logic;
 		red, green, blue: out std_logic_vector(9 downto 0);
 
-		speed: in std_logic_vector(9 downto 0);
-		x: in std_logic;
-		speedout: out std_logic_vector(9 downto 0);
+		fourKey: in std_logic_vector(3 downto 0);
 		
 		hsync, vsync, blank: out std_logic;
 		clk_vga: out std_logic;
@@ -21,26 +19,24 @@ end entity;
 architecture controle of vga is
 	signal cmpt_pixel, cmpt_ligne: std_logic_vector(9 downto 0);
 	signal enable_ligne, div2, div2int: std_logic;
-
-	signal xOffset: std_logic_vector(9 downto 0);
-	signal speedy: std_logic;
-	signal speedint: std_logic_vector(9 downto 0);
 	
+	signal carre0: std_logic;
+	signal xOffset, yOffset: std_logic_vector(9 downto 0);
+	signal humanClock, x, y: std_logic;
+	signal brain, bodyWave: std_logic_vector(3 downto 0);
 begin
---	
---	U0: entity work.carre(Behavioral)
---				port map(
---					cmpt_pixel => cmpt_pixel,
---					cmpt_ligne => cmpt_ligne,
---					cote => std_logic_vector(to_unsigned(10,10)),
---					positionX => std_logic_vector(unsigned(offset)),
---					positionY => std_logic_vector(to_unsigned(225,10)),
---					interieur => carre0
---				)
---		;
---		
---		
---		
+	
+	U0: entity work.carre(Behavioral)
+			port map(
+				cmpt_pixel => cmpt_pixel,
+				cmpt_ligne => cmpt_ligne,
+				cote => std_logic_vector(to_unsigned(10,10)),
+				positionX => xOffset,
+				positionY => yOffset,
+				interieur => carre0
+			)
+		;
+
 --	U1: entity work.carre(Behavioral)
 --				port map(
 --					cmpt_pixel => cmpt_pixel,
@@ -117,7 +113,7 @@ begin
 				port map(
 					clk => clk,
 					resetn => raza,
-					en_user => speedy
+					en_user => humanClock
 				)
 		;
 		
@@ -129,9 +125,22 @@ begin
 					port map(
 						clk => clk,
 						raza => raza,
-						en => speedy,
+						en => humanClock,
 						sens => x,
 						val => xOffset
+					)
+		;
+		
+		Yaxis: entity work.inversibleCompteur(Behavioral)
+					generic map(
+						n => 7
+					)
+					port map(
+						clk => clk,
+						raza => raza,
+						en => humanClock,
+						sens => y,
+						val => yOffset
 					)
 		;
 		
@@ -141,8 +150,54 @@ begin
 --					)
 --		;
 --		
-	speedint(0) <= not speedint(0) when rising_edge(speedy);
-	speedout <= xOffset;
+
+	buttonUp: entity work.button(Behavioral)
+		port map(
+			clk => clk,
+			button => fourKey(0),
+			trigger => brain(0)
+		)
+	;
+	
+	buttonDown: entity work.button(Behavioral)
+		port map(
+			clk => clk,
+			button => fourKey(1),
+			trigger => brain(1)
+		)
+	;
+	
+	buttonLeft: entity work.button(Behavioral)
+		port map(
+			clk => clk,
+			button => fourKey(2),
+			trigger => brain(2)
+		)
+	;
+	
+	buttonRigth: entity work.button(Behavioral)
+		port map(
+			clk => clk,
+			button => fourKey(3),
+			trigger => brain(3)
+		)
+	;
+	
+
+	snakeHead: entity work.headControle(stateMachine)
+		port map(
+			l => brain(0),
+			r => brain(1),
+			u => brain(2),
+			d => brain(3),
+			clk => clk,
+			raza => raza,
+			Gauche => bodyWave(0),
+			Droite => bodyWave(1),
+			Haut => bodyWave(2),
+			Bas => bodyWave(3)
+		)
+	;	
 	
 	enable_ligne <= '1' when div2 = '1' and unsigned(cmpt_pixel) = 0 else '0';
 
@@ -150,11 +205,12 @@ begin
 	div2 <= '0' when raza = '0' else div2int when rising_edge(clk);
 	clk_vga <= div2;
 
-
+	x <= '1' when bodyWave(1) = '1' else '0';
+	y <= '1' when bodyWave(3) = '1' else '0';
 	
-	--red <= (others => '0');
-	--green <= (others => '1');
-	--blue <= (others => '0');
+	red <= (others => '0') when carre0 = '1' else (others => '1');
+	green <= (others => '1');
+	blue <= (others => '0');
 	
 	
 end controle;
